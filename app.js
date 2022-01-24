dc14059a28102bf0473b532a2ac51ddeaecb2705
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-24 19:26:58 +0700

    fetchall products

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const shopRoutes = require("./routes/shop");
 
 const errorControllers = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorControllers.get404);
 
