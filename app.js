d64ce732d118b8bd0303588509c136fddfd53e80
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-24 10:16:50 +0700

    cart functionality

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const errorControllers = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -37,6 +39,10 @@ app.use(errorControllers.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true })
@@ -53,6 +59,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(8000);
   })
   .catch((err) => console.log(err));
