1f845a309467ffe5d08626c4fae855fc0ea384b8
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-29 20:37:21 +0700

    login page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 const errorControllers = require("./controllers/error");
 const User = require("./models/user");
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorControllers.get404);
 
