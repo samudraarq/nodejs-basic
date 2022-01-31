19f7f0305e5f9684cabd9a1b42109892f6ae1e1e
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-31 19:26:11 +0700

    install mongodb session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -14,7 +15,13 @@ const authRoutes = require("./routes/auth");
 const errorControllers = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = `mongodb+srv://admin-samudra:${process.env.MONGODB_PASS}@cluster0.cd33k.mongodb.net/shop?retryWrites=true&w=majority`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -22,7 +29,12 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
@@ -41,9 +53,7 @@ app.use(authRoutes);
 app.use(errorControllers.get404);
 
 mongoose
-  .connect(
-    `mongodb+srv://admin-samudra:${process.env.MONGODB_PASS}@cluster0.cd33k.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
