894a4248fdead0088bccb36d257b61f35f4b219a
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-02-02 13:27:30 +0700

    csrf protection

--- controllers/shop.js
+++ controllers/shop.js
@@ -35,7 +35,6 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "Shop",
         path: "/",
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -87,7 +86,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user,
         },
         products: products,
